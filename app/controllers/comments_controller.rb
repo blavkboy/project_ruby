02548bec80bf3ead041dd9fa68dcdc846ec81f68
Gladwin and Mmangaliso <g.mohlamonyane@gmail.com>
2017-12-11 10:41:06 +0000

    added feature to delete comments.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,6 +9,14 @@ class CommentsController < ApplicationController
     redirect_to article_path(@comment.article)
   end
 
+  def destroy
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+    @comment.destroy
+    flash.notice = "Deleted comment"
+    redirect_to article_path(@article)
+  end
+
   def comment_params
     params.require(:comment).permit(:author_name, :body)
   end
